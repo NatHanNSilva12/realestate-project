9e64216a9498dfef4dbc9de2a0e2e46cb2f20abd
NatHan <nathannssilva12@gmail.com>
2024-07-11 10:45:23 -0300

    first commit

---
+++ api/app.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http';
+import express from "express";
+import authRoute from "./routes/auth.route.js"
+import postRoute from "./routes/post.route.js"
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.use("/api/test", (req, res)=>{
+    res.send("Funciona!")
+})
+
+app.listen(8800, () => {
+    console.log("Servidor rodando!");
+});
+
---
+++ api/controllers/auth.controllers.js
@@ -0,0 +1,12 @@
+export const register = (req, res)=>{
+     //db
+     console.log(req.body) 
+}
+
+export const login = (req, res)=>{
+    //db
+}
+
+export const logout = (req, res)=>{
+    //db
+}
