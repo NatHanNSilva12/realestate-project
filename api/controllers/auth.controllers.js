ccf67bd89bb65a7282f0fd1978189b918f31cb93
NatHan <nathannssilva12@gmail.com>
2024-07-16 12:59:09 -0300

    importando o prisma na autenticação e criação de usuarios

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,13 +1,20 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 
 export const register = async (req, res)=>{
      //db
      const { username, email, password } = req.body; 
 
+     //criptografar a senha em hash
+
      const hashedPassword = await bcrypt.hash(password, 10);
 
      console.log(hashedPassword)
+
+     //criar usuario em mongodb e salvar 
+
+     const newUser = await prisma
 }
 
 export const login = (req, res)=>{
